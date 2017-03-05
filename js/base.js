65ef28401ad7b0e961821eb056b0a3e61748da53
博文 <z.bowen66@gmail.com>
2017-03-05 18:09:10 +0800

    增加删除前确认，并解决bug

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 ;(function () {
     'use strict';
-
+    // store.clear();
     // var task_list={};
     var task_list=[];
     // var new_task={};
@@ -25,10 +25,12 @@
     }
     function listen(i) {
         $('#del_' + i).on('click',function () {
-                task_list[i]=i;
-                // console.log(i);
-                delete_task(i);
-            } );
+                console.log("点",task_list[i]);
+                var _r=confirm("确定删除？");
+                _r?delete_task(i):null;
+                // if(_r) {delete_task(i) }
+                // delete_task(i);
+        } );
     }
     function render_task(i) {
         $('.task-list').prepend(
@@ -50,7 +52,7 @@
         new_task.content=$(this).find('input[name=content]').val();//获取输入内容
         if(!new_task.content) return;//如果内容为空，则停止
         // task_list.push(new_task);//add_task()做过了
-        console.log('new_task',new_task);
+        console.log('新',new_task);
         if(add_task(new_task)){
             render_task_list();
         };
@@ -64,8 +66,9 @@
     //清除函数
     function delete_task(_index) {
         // if(_index===undefined||!task_list[_index]) return;
-        // delete task_list[index];
-        console.log('delete_task',task_list.splice(_index,1)[0]);
+        // delete task_list[_index];
+        var _d=task_list.splice(_index,1);
+        console.log('删除的_d[0].content',_d[0].content);
         refresh_list();
     }
 
