f0ba889a0c939e614b22b5ca08937a0560ca9367
博文 <z.bowen66@gmail.com>
2017-03-06 12:38:01 +0800

    完善了增删改功能

--- js/base.js
+++ js/base.js
@@ -7,11 +7,12 @@
     init();
     function init() {
         task_list=store.get('task_list')||[];
+        console.log('当前任务列表',task_list);
         render_task_list();
     }
     function refresh_list() {
         store.set('task_list',task_list);
-        console.log('task_list',task_list);
+        console.log('当前任务列表task_list',task_list);
         render_task_list();
     }
     function add_task(nt) {
@@ -22,11 +23,40 @@
     }
     function listen(i) {
         $('#del_' + i).on('click',function () {
-                console.log("点",task_list[i]);
+                console.log("点击了删除按钮",task_list[i]);
                 var _r=confirm("确定删除？");
-                _r?delete_task(i):null;           // if(_r) {delete_task(i)
+                _r?delete_task(i):console.log("取消了删除操作");
         } );
+        $('#det_'+i).on('click',function () {
+            show_detail(i);
+            console.log('查看了任务',task_list[i])
+        })
     }
+    function show_detail(i){
+        $('.task-detail').show();$('.task-detail-mask').show();
+        var $cont=$('h2.content'),
+                $desc=$('textarea[name=desc]'),
+                $date=$('input[name=date]'),
+                $time=$('input[name=time]');
+        $cont.text(task_list[i].content);
+        $desc.val(task_list[i].desc);
+        $date.val(task_list[i].date);
+        $time.val(task_list[i].time);
+            $('.remind').on('submit',function (e) {
+                e.preventDefault();
+                task_list[i].desc=$desc.val();
+                task_list[i].content=$cont.text();
+                task_list[i].date=$date.val();
+                task_list[i].time=$time.val();
+                console.log('任务内容更新为',task_list[i]);
+                refresh_list();
+                $('.task-detail-mask').click();
+            });
+    }
+    $('.task-detail-mask').on('click',function () {
+        $(this).hide();$('.task-detail').hide();
+        $('.remind').unbind('submit');
+    });
     function render_task(i) {
         $('.task-list').prepend($(
             _tpl.replace('{{item-content}}',task_list[i].content)
@@ -48,10 +78,11 @@
         new_task.content=$(this).find('input[name=content]').val();//获取输入内容
         if(!new_task.content) return;//如果内容为空，则停止
         // task_list.push(new_task);//add_task()做过了
-        console.log('新',new_task);
+        console.log('添加了一个新任务',new_task);
         if(add_task(new_task)){
             render_task_list();
         }
+        $('input[name=content]').val('');
     });
     //清除全部任务
     $('h1').on('dblclick',function (e) {
@@ -64,7 +95,7 @@
         // if(_index===undefined||!task_list[_index]) return;
         // delete task_list[_index];
         var _d=task_list.splice(_index,1);
-        console.log('删除的_d[0].content',_d[0].content);
+        console.log('删除了任务',_d[0]);
         refresh_list();
     }
 
